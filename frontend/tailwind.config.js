ec0b36aac14e8245a668d375753f65caf027e889
glyphic-sentry <eli.hamel@sentry.io>
2025-07-20 14:17:33 -0700

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,14 @@
+// tailwind.config.js
 module.exports = {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './views/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
