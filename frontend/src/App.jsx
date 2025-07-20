ca7b8016a65f9091ebccb6aaeee60cf60f31822b
glyphic-sentry <eli.hamel@sentry.io>
2025-07-20 15:08:52 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import GalleryView from "../views/GalleryView";
 import ListView from "../views/ListView";
@@ -12,7 +11,9 @@ export default function App() {
         <Link to="/list">List</Link>
         <Link to="/report">Reports</Link>
       </nav>
+
       <Routes>
+        {/* Home route shows the gallery */}
         <Route path="/" element={<GalleryView />} />
         <Route path="/list" element={<ListView />} />
         <Route path="/report" element={<ReportView />} />
