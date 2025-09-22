c8686db672a27dea6528cae3d9f28644a799c105
glyphic-sentry <eli.hamel@sentry.io>
2025-09-22 06:48:55 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// frontend/src/App.jsx
 import { Routes, Route, Link } from "react-router-dom";
 import GalleryView from "./views/GalleryView";
 import ListView from "./views/ListView";
@@ -7,14 +6,14 @@ import ReportView from "./views/ReportView";
 export default function App() {
   return (
     <>
-      {/* Sticky navbar: stays fixed at the top */}
+      {/* Sticky navigation bar */}
       <nav className="sticky top-0 bg-gray-800 text-white flex gap-4 p-4 shadow">
         <Link to="/">Gallery</Link>
         <Link to="/list">List</Link>
         <Link to="/report">Reports</Link>
       </nav>
 
-      {/* Routed pages take up the rest of the space */}
+      {/* Route definitions */}
       <Routes>
         <Route path="/" element={<GalleryView />} />
         <Route path="/list" element={<ListView />} />
