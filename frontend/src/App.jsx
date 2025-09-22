9755012aec7e4187597369d70e87ae18e6dea7dc
glyphic-sentry <eli.hamel@sentry.io>
2025-09-21 22:14:37 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,25 @@
+// frontend/src/App.jsx
 import { Routes, Route, Link } from "react-router-dom";
-import GalleryView from "../views/GalleryView";
-import ListView from "../views/ListView";
-import ReportView from "../views/ReportView";
+import GalleryView from "./views/GalleryView";
+import ListView from "./views/ListView";
+import ReportView from "./views/ReportView";
 
 export default function App() {
   return (
-    <div className="bg-gray-900 text-white min-h-screen flex flex-col">
-      {/* Sticky navbar: stays fixed at the top and doesn’t affect page scrolling */}
-      <nav className="p-4 flex gap-4 bg-gray-800 shadow sticky top-0 z-10 flex-shrink-0">
+    <>
+      {/* Sticky navbar: stays fixed at the top */}
+      <nav className="sticky top-0 bg-gray-800 text-white flex gap-4 p-4 shadow">
         <Link to="/">Gallery</Link>
         <Link to="/list">List</Link>
         <Link to="/report">Reports</Link>
       </nav>
 
-      {/* Routed pages take up the rest of the vertical space */}
-      <div className="flex-grow">
-        <Routes>
-          <Route path="/" element={<GalleryView />} />
-          <Route path="/list" element={<ListView />} />
-          <Route path="/report" element={<ReportView />} />
-        </Routes>
-      </div>
-    </div>
+      {/* Routed pages take up the rest of the space */}
+      <Routes>
+        <Route path="/" element={<GalleryView />} />
+        <Route path="/list" element={<ListView />} />
+        <Route path="/report" element={<ReportView />} />
+      </Routes>
+    </>
   );
 }
